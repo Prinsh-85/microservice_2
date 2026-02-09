cbf51d46e5dc38c44d9c3d29e514f4c073712214
Prinsh-85 <prinshagnihotri67@gmail.com>
2026-02-09 14:19:05 +0530

    thirs api addon

--- UserService (1)/UserService/src/main/java/com/example/user/service/Controller/EmployeeController.java
+++ UserService (1)/UserService/src/main/java/com/example/user/service/Controller/EmployeeController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
@@ -25,5 +27,12 @@ public class EmployeeController {
     public ResponseEntity<Employee> getById(@PathVariable String id){
         return ResponseEntity.ok(service.getById(id));
     }
+
+    //get all
+    @GetMapping
+    public  ResponseEntity<List<Employee>> getAll(){
+        return ResponseEntity.ok(service.getAll());
+    }
+
 
 }
