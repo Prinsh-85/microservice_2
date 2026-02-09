9e899bdf0ba39102e21827e50fee6144545cef2c
Prinsh-85 <prinshagnihotri67@gmail.com>
2026-02-09 14:50:08 +0530

    do something

--- UserService (1)/UserService/src/main/java/com/example/user/service/service/impl/EmployeeServiceImpl.java
+++ UserService (1)/UserService/src/main/java/com/example/user/service/service/impl/EmployeeServiceImpl.java
@@ -5,9 +5,10 @@ import com.example.user.service.entity.Employee;
 import com.example.user.service.service.EmployeeService;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
 
     @Autowired
